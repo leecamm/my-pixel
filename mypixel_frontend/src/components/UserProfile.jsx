05db4a1675a783ff26e213a827b364c2e173b3aa
Le <hieu.le@accenture.com>
2022-01-05 19:52:03 +0100

    feat: create profile page

--- mypixel_frontend/src/components/UserProfile.jsx
+++ mypixel_frontend/src/components/UserProfile.jsx
@@ -1,7 +1,145 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { AiOutlineLogout } from "react-icons/ai";
+import { useParams, useNavigate } from "react-router-dom";
+import { GoogleLogout } from "react-google-login";
+
+import {
+  userCreatedPinsQuery,
+  userQuery,
+  userSavedPinsQuery,
+} from "../utils/data";
+import { client } from "../client";
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
+
+const randomImage =
+  "https://source.unsplash.com/1600x900/?nature,photography,technology";
+
+const activeBtnStyles =
+  "bg-emerald-400 text-white font-semibold p-2 rounded-full w-20 outline-none";
+const notActiveBtnStyles =
+  "bg-primary text-black mr-4 font-bold p-2 rounded-full w-20 outline-none";
 
 const UserProfile = () => {
-  return <div>UserProfile</div>;
+  const [user, setUser] = useState(null);
+  const [pins, setPins] = useState(null);
+  const [text, setText] = useState("Created");
+  const [activeBtn, setActiveBtn] = useState("created");
+  const navigate = useNavigate();
+  const { userId } = useParams();
+
+  const logout = () => {
+    localStorage.clear();
+
+    navigate("/login");
+  };
+
+  useEffect(() => {
+    const query = userQuery(userId);
+
+    client.fetch(query).then((data) => {
+      setUser(data[0]);
+    });
+  }, [userId]);
+
+  useEffect(() => {
+    if (text === "Created") {
+      const createdPinsQuery = userCreatedPinsQuery(userId);
+
+      client.fetch(createdPinsQuery).then((data) => {
+        setPins(data);
+      });
+    } else {
+      const savePinsQuery = userSavedPinsQuery(userId);
+
+      client.fetch(savePinsQuery).then((data) => {
+        setPins(data);
+      });
+    }
+  }, [text, userId]);
+
+  if (!user) {
+    return <Spinner message="Loading your profile..." />;
+  }
+
+  return (
+    <div className="relative pb-2 h-full justify-center items-center">
+      <div className="flex flex-col pb-5">
+        <div className="relative flex flex-col mb-7">
+          <div className="flex flex-col justify-center items-center p-2">
+            <img
+              src={randomImage}
+              className="w-full h-370 2xl:h-510 shadow-lg object-cover rounded-xl"
+              alt="banner-pic"
+            />
+            <img
+              className="rounded-full w-25 h-25 -mt-12 shadow-xl object-cover"
+              src={user.image}
+              alt="user-avatar"
+            />
+            <h1 className="font-bold text-2xl text-center mt-3">
+              {user.userName}
+            </h1>
+            <div className="absolute top-2 z-10 right-2 p-2">
+              {userId === user._id && (
+                <GoogleLogout
+                  clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
+                  render={(renderProps) => (
+                    <button
+                      type="button"
+                      className="bg-white p-2 rounded-full cursor-pointer outline-none shadow-md hover:bg-gray-50 hover:shadow-xl"
+                      onClick={renderProps.onClick}
+                      disabled={renderProps.disabled}
+                    >
+                      <AiOutlineLogout color="red" fontSize={21} />
+                    </button>
+                  )}
+                  onLogoutSuccess={logout}
+                  cookiePolicy="single_host_origin"
+                />
+              )}
+            </div>
+          </div>
+          <div className="text-center mb-7 space-x-5">
+            <button
+              type="button"
+              onClick={(e) => {
+                setText(e.target.textContent);
+                setActiveBtn("created");
+              }}
+              className={`${
+                activeBtn === "created" ? activeBtnStyles : notActiveBtnStyles
+              }`}
+            >
+              Created
+            </button>
+            <button
+              type="button"
+              onClick={(e) => {
+                setText(e.target.textContent);
+                setActiveBtn("saved");
+              }}
+              className={`${
+                activeBtn === "saved" ? activeBtnStyles : notActiveBtnStyles
+              }`}
+            >
+              Saved
+            </button>
+          </div>
+
+          {pins?.length ? (
+            <div className="px-2">
+              <MasonryLayout pins={pins} />
+            </div>
+          ) : (
+            <div className="flex justify-center font-bold items-center w-full text-xl mt-2">
+              No Photos Found!
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UserProfile;
